4733f4594c08a0379f0fea254f6a8c0114c106c6
Lindsey Armstrong <linmicarm@gmail.com>
2026-07-12 18:04:30 -0400

    fixing render deployment

--- client/src/socket.ts
+++ client/src/socket.ts
@@ -1,7 +1,8 @@
 import { io, type Socket } from "socket.io-client";
 import type { ClientToServerEvents, ServerToClientEvents } from "./types";
 
-export const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io(
-  "http://localhost:3001",
-  { autoConnect: false }
-);
+const URL = import.meta.env.VITE_SERVER_URL ?? "http://localhost:3001";
+
+export const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io(URL, {
+  autoConnect: false,
+});
